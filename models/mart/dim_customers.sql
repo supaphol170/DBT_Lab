4b7c5b0d90bc6e2e32404ea3bc31959f5a6e8d81
Supaphol Kodtom <supaphol.kodtom@gmail.com>
2026-03-06 08:20:48 +0000

    update null value in payment

--- models/mart/dim_customers.sql
+++ models/mart/dim_customers.sql
@@ -3,13 +3,23 @@ WITH customers AS (
     FROM {{ ref('stg_jaffle_shop__customers') }}
 ),
 orders AS (
-    SELECT *
+    SELECT customer_id,
+        COUNT(orders_id) AS number_of_orders,
+        MIN(order_date) AS first_order_date,
+        MAX(order_date) AS most_recent_order_date
     FROM {{ ref('stg_jaffle_shop__orders') }}
+    GROUP BY 1
 ),
 final AS (
-    SELECT *
+    SELECT customer_id,
+        first_name,
+        last_name,
+        COALESCE(number_of_orders, 0) AS number_of_orders,
+        COALESCE(first_order_date, '1900-01-01') AS first_order_date,
+        COALESCE(most_recent_order_date, '1900-01-01') AS most_recent_order_date
     FROM customers
-    INNER JOIN orders USING (customer_id)
+    LEFT JOIN orders USING (customer_id)
 )
 SELECT *
 FROM final
+ORDER BY customer_id
